657565b3ae019aeff1964258a42e96a974487c4e
Vitalii Klimov <vitklim11@gmail.com>
2022-07-18 10:47:54 +0300

    TextView

--- app/src/main/java/com/example/techdemo/MainActivity.java
+++ app/src/main/java/com/example/techdemo/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.techdemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = (TextView) findViewById(R.id.text_view);
+
+        String textFromTextView = textView.getText().toString();
+        Log.d(TAG, "textFromTextView orig = [" + textFromTextView + "]");
+
+        textView.setText("Set TextView");
+        textFromTextView = textView.getText().toString();
+        Log.d(TAG, "textFromTextView set = [" + textFromTextView + "]");
     }
 }
